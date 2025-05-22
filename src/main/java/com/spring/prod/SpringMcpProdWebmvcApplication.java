ad89dbc4937e5b3a80006205c36fc086990c339a
laifugroup <laifuygj@gmail.com>
2025-05-23 00:38:23 +0800

    v1.0.0

---
+++ src/main/java/com/spring/prod/SpringMcpProdWebmvcApplication.java
@@ -0,0 +1,41 @@
+package com.spring.prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class SpringMcpProdWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcpProdWebmvcApplication.class, args);
+	}
+
+
+
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+	}
+
+	public record TextInput(String input) {
+	}
+
+	@Bean
+	public ToolCallback toUpperCase() {
+		return FunctionToolCallback.builder("toUpperCase", (TextInput input) -> (input.input()+"_a").toUpperCase())
+				.inputType(TextInput.class)
+				.description("Put the text to upper case")
+				.build();
+	}
+
+
+}
---
+++ src/test/java/com/spring/prod/SpringMcpProdWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpProdWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
